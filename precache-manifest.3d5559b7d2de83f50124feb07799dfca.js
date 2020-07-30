a7677e413c68ab85daf9f0d2c21dd461f4cb7e88
kinlextech <laithong865@gmail.com>
2020-07-31 02:20:15 +0700

    deploy

---
+++ precache-manifest.3d5559b7d2de83f50124feb07799dfca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51688708bcbde7f6f7a5",
+    "url": "/css/app.f97b1672.css"
+  },
+  {
+    "revision": "eed7c3e76af3db5b3333",
+    "url": "/css/chunk-vendors.1e103cac.css"
+  },
+  {
+    "revision": "5a37fdea2d24a8b52af9d84daa0e6226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51688708bcbde7f6f7a5",
+    "url": "/js/app.b5e94f76.js"
+  },
+  {
+    "revision": "eed7c3e76af3db5b3333",
+    "url": "/js/chunk-vendors.647eecd7.js"
+  },
+  {
+    "revision": "9a8190d55d1ecfcdf3bf8913a16239f1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
